c2e93ed34db8b1ffc25666ce5aa1061f0c5624fa
micascheid <mica.linscheid@western.edu>
2018-09-13 18:39:59 -0600

    final

--- colorize.js
+++ colorize.js
@@ -1,16 +1,14 @@
 function correct(){
     var body = document.getElementById("main");
     var description = document.getElementById("description");
-    body.classList.add("backGreen");
+    body.className = "backGreen"
     description.style.visibility = "visible"
-
-
 }
 
 function wrong() {
     var body = document.getElementById("main");
     var description = document.getElementById("description");
-    body.classList.add("backRed")
+    body.className = "backRed"
     description.style.visibility = "hidden"
     document.getElementsByClassName("correct")
 }
